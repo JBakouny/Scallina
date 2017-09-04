6311498d296ecb2987f18a796912f795ab618e18
Joseph Bakouny <youssef.bakouny@usj.edu.lb>
2017-09-04 14:35:39 +0300

    Add QueueRecord Coq file and manually written expected Scala code

---
+++ src/test/resources/QueueRecord.v
@@ -0,0 +1,56 @@
+(*
+Inspired from:
+https://github.com/parametricity-coq/paramcoq/blob/master/test-suite/ListQueue.v
+*)
+
+Require Import Coq.Lists.List.
+
+Record Queue :=
+{
+  T : Type;
+  empty : T;
+  push : nat -> T -> T;
+  pop : T -> option (nat * T)
+}.
+
+
+Definition ListQueue := Build_Queue
+  (list nat) 
+  nil 
+  (fun (x : nat) (l : list nat) => x :: l)
+  (fun l => 
+    match rev l with
+      | nil => None 
+      | hd :: tl => Some (hd, rev tl) end)
+.
+
+Definition DListQueue := Build_Queue
+  (list nat * list nat) 
+  (nil, nil)
+  (fun (x : nat) (l : list nat * list nat) => 
+    let (back, front) := l in 
+    (cons x back,front))
+  (fun l =>
+    let (back, front) := l in 
+    match front with 
+      | nil => 
+         match rev back with
+            | nil => None
+            | hd :: tl => Some (hd, (nil, tl))
+         end
+      | hd :: tl => Some (hd, (back, tl))
+    end)
+.
+
+Fixpoint insertElems (Q: Queue) (q: Q.(T)) (n: nat) : Q.(T) :=
+match n with
+  0 => q
+| S m => Q.(push) n (insertElems Q q m)
+end.
+
+Definition createQueue (Q: Queue) (n: nat) : Q.(T) := insertElems Q Q.(empty) n.
+
+Definition createListQueue(n: nat) := createQueue ListQueue n.
+
+Definition createDListQueue(n: nat) := createQueue DListQueue n.
+
---
+++ src/test/resources/scala/of/coq/generated/code/QueueRecord.scala
@@ -0,0 +1,35 @@
+import scala.of.coq.lang._
+import Nat._
+import MoreLists._
+
+object QueueRecord {
+  trait Queue {
+    type T
+    def empty: T
+    def push: Nat => T => T
+    def pop: T => Option[(Nat, T)]
+  }
+
+  def Build_Queue[Queue_T](Queue_empty: Queue_T)(Queue_push: Nat => Queue_T => Queue_T)(Queue_pop: Queue_T => Option[(Nat, Queue_T)]): Queue = {
+    new Queue {
+      type T = Queue_T
+      def empty = Queue_empty
+      def push = Queue_push
+      def pop = Queue_pop
+    }
+  }
+
+  def ListQueue = Build_Queue[List[Nat]](Nil)((x: Nat) => (l: List[Nat]) => x :: l)(l => rev(l) match {
+    case Nil      => None
+    case hd :: tl => Some(hd, rev(tl))
+  })
+
+  def insertElems(Q: Queue)(q: Q.T)(n: Nat): Q.T = n match {
+    case Zero => q
+    case S(m) => Q.push(n)(insertElems(Q)(q)(m))
+  }
+
+  def createQueue(Q: Queue)(n: Nat): Q.T = insertElems(Q)(Q.empty)(n)
+
+  def createListQueue(n: Nat) = createQueue(ListQueue)(n)
+}
