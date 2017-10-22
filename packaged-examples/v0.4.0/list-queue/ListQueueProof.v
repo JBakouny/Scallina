f1a2ec45c75846a95020400ddb3e3eee1ee65d60
Joseph Bakouny <youssef.bakouny@usj.edu.lb>
2017-10-22 17:25:08 +0300

    aesthetic modifications to v0.4.0/list-queue/ListQueueProof.v

--- packaged-examples/v0.4.0/list-queue/ListQueueProof.v
+++ packaged-examples/v0.4.0/list-queue/ListQueueProof.v
@@ -25,14 +25,12 @@ Defined.
 
 Parametricity Recursive option.
 
-Lemma option_nat_R_equal : 
-  forall x y, option_R nat nat nat_R x y -> x = y.
+Lemma option_nat_R_equal : forall x y, option_R nat nat nat_R x y -> x = y.
 intros x1 x2 H; destruct H as [x1 x2 x_R | ].
 rewrite (nat_R_equal _ _ x_R); reflexivity.
 reflexivity.
 Defined.
-Lemma equal_option_nat_R : 
-  forall x y, x = y -> option_R nat nat nat_R x y.
+Lemma equal_option_nat_R : forall x y, x = y -> option_R nat nat nat_R x y.
 intros x y H; subst.
 destruct y; constructor; apply equal_nat_R; reflexivity.
 Defined.
@@ -50,19 +48,17 @@ Print Queue_R.
 
 Definition R (l1 : list nat) (l2 : list nat * list nat) :=
  let (back, front) := l2 in 
-  l1 = back ++ rev front.
+  l1 = app back (rev front).
 
-Lemma rev_app : 
-  forall A (l1 l2 : list A), 
-    rev (l1 ++ l2) = rev l2 ++ rev l1.
+Lemma rev_app : forall A (l1 l2 : list A), 
+    rev (app l1 l2) = app (rev l2) (rev l1).
 induction l1.
 intro; symmetry; apply app_nil_r.
 intro; simpl; rewrite IHl1; rewrite app_ass.
 reflexivity.
 Defined.
 
-Lemma rev_list_rect A  :
-      forall P:list A-> Type,
+Lemma rev_list_rect A : forall P:list A-> Type,
 	P nil ->
 	(forall (a:A) (l:list A), P (rev l) -> P (rev (a :: l))) ->
 	forall l:list A, P (rev l).
@@ -70,10 +66,9 @@ Proof.
   induction l; auto.
 Defined.
 
-Theorem rev_rect A :
-  forall P:list A -> Type,
+Theorem rev_rect A : forall P:list A -> Type,
 	P nil ->
-	(forall (x:A) (l:list A), P l -> P (l ++ x :: nil)) ->
+	(forall (x:A) (l:list A), P l -> P (app l (x :: nil))) ->
   forall l:list A, P l.
 Proof.
   intros.
@@ -147,8 +142,7 @@ Check program.
 Parametricity Recursive program.
 Check program_R.
 
-Lemma program_independent : 
- forall n, 
+Lemma program_independent : forall n, 
   program ListQueue n = program DListQueue n.
 intro n.
 apply option_nat_R_equal.
