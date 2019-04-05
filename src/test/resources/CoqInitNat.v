cbfdcf125261a6961cd834b91d9d0c048737358a
Joseph Bakouny <youssef.bakouny@usj.edu.lb>
2019-04-05 16:57:33 +0300

    Translate all of Coq.Init.Nat from Gallina to Scala

--- src/test/resources/CoqInitNat.v
+++ src/test/resources/CoqInitNat.v
@@ -1,3 +1,11 @@
+(*
+
+Replace 0 by Zero.
+Replace ' by 1 in qualids.
+Inline notations.
+Add types to method signatures.
+*)
+
 Inductive Nat := Zero | S (n : Nat).
 
 Definition pred (n : Nat) : Nat :=
@@ -12,6 +20,8 @@ Fixpoint add (n m : Nat) : Nat :=
   | S p => S (add p m)
   end.
 
+Definition double (n : Nat) : Nat := add n n.
+
 Fixpoint mul (n m : Nat) : Nat :=
   match n with
   | Zero => Zero
@@ -23,4 +33,180 @@ Fixpoint sub (n m : Nat) : Nat :=
   | S k, S l => sub k l
   | _, _ => n
   end.
+
+Fixpoint eqb (n m : Nat) : bool :=
+  match n, m with
+    | Zero, Zero => true
+    | Zero, S _ => false
+    | S _, Zero => false
+    | S n1, S m1 => eqb n1 m1
+  end.
+
+Fixpoint leb (n m : Nat) : bool :=
+  match n, m with
+    | Zero, _ => true
+    | _, Zero => false
+    | S n1, S m1 => leb n1 m1
+  end.
+
+Definition ltb (n m : Nat) := leb (S n) m.
+
+Fixpoint compare (n m : Nat) : comparison :=
+  match n, m with
+   | Zero, Zero => Eq
+   | Zero, S _ => Lt
+   | S _, Zero => Gt
+   | S n1, S m1 => compare n1 m1
+  end.
+
+
+Fixpoint max (n m : Nat) : Nat :=
+  match n, m with
+    | Zero, _ => m
+    | S n1, Zero => n
+    | S n1, S m1 => S (max n1 m1)
+  end.
+
+Fixpoint min (n m : Nat) : Nat :=
+  match n, m with
+    | Zero, _ => Zero
+    | S n1, Zero => Zero
+    | S n1, S m1 => S (min n1 m1)
+  end.
+
+Fixpoint even (n : Nat) : bool :=
+  match n with
+    | Zero => true
+    | S Zero => false
+    | S (S n1) => even n1
+  end.
+
+Definition odd (n : Nat) : bool := negb (even n).
+
+Fixpoint pow (n m : Nat) : Nat :=
+  match m with
+    | Zero => S Zero
+    | S m => mul n (pow n m)
+  end.
+
+Fixpoint tail_add (n m : Nat) : Nat :=
+  match n with
+    | Zero => m
+    | S n => tail_add n (S m)
+  end.
+
+Fixpoint tail_addmul (r n m : Nat) : Nat :=
+  match n with
+    | Zero => r
+    | S n => tail_addmul (tail_add m r) n m
+  end.
+
+(* Disregard the following functions since they use
+Decimals which are not currently supported by Scallina:
+- of_uint_acc
+- of_uint
+- to_little_uint
+- to_uint
+- of_int
+- to_int
+*)
+
+Fixpoint divmod (x y q u : Nat) : Nat * Nat :=
+  match x with
+    | Zero => (q,u)
+    | S x1 => match u with
+                | Zero => divmod x1 y (S q) y
+                | S u1 => divmod x1 y q u1
+              end
+  end.
+
+Definition div (x y : Nat) : Nat :=
+  match y with
+    | Zero => y
+    | S y1 => fst (divmod x y1 Zero y1)
+  end.
+
+Definition modulo (x y : Nat) :=
+  match y with
+    | Zero => y
+    | S y1 => sub y1 (snd (divmod x y1 Zero y1))
+  end.
+
+Fixpoint gcd (a b : Nat) : Nat :=
+  match a with
+   | Zero => b
+   | S a1 => gcd (modulo b (S a1)) (S a1)
+  end.
+
+Definition square (n : Nat) : Nat := mul n n.
+
+Fixpoint sqrt_iter (k p q r : Nat) : Nat :=
+  match k with
+    | Zero => p
+    | S k1 => match r with
+                | Zero => sqrt_iter k1 (S p) (S (S q)) (S (S q))
+                | S r1 => sqrt_iter k1 p q r1
+              end
+  end.
+
+Definition sqrt (n : Nat) : Nat := sqrt_iter n Zero Zero Zero.
+
+
+Fixpoint log2_iter (k p q r : Nat) : Nat :=
+  match k with
+    | Zero => p
+    | S k1 => match r with
+                | Zero => log2_iter k1 (S p) (S q) q
+                | S r1 => log2_iter k1 p (S q) r1
+              end
+  end.
+
+Definition log2 (n : Nat) : Nat := log2_iter (pred n) Zero (S Zero) Zero.
+
+(* Replace iter by a version that does not use the dependently typed nat_rect function, see the result of "Extraction iter" for OCaml *)
+Fixpoint iter {A} (n:Nat) (f:A->A) (x:A) : A :=
+ match n with
+  | Zero => x
+  | S n0 => f (iter n0 f x)
+ end.
+
+Fixpoint div2 (n : Nat) : Nat :=
+  match n with
+  | Zero => Zero
+  | S Zero => Zero
+  | S (S n1) => S (div2 n1)
+  end.
+
+Fixpoint testbit (a n : Nat) : bool :=
+ match n with
+   | Zero => odd a
+   | S n => testbit (div2 a) n
+ end.
+
+
+(* Replace shiftl by a version that does not use the dependently typed nat_rect function, see the result of "Extraction iter" for OCaml *)
+Fixpoint shiftl (a n: Nat) : Nat :=
+ match n with
+   | Zero => a
+   | S n0 => double (shiftl a n0)
+ end.
+
+Fixpoint shiftr (a n: Nat) : Nat :=
+ match n with
+   | Zero => a
+   | S n0 => div2 (shiftr a n0)
+ end.
+
+Fixpoint bitwise (op:bool->bool->bool) (n a b : Nat) : Nat :=
+ match n with
+  | Zero => Zero
+  | S n1 =>
+    add (if op (odd a) (odd b) then (S Zero) else Zero)
+    (mul (S (S Zero)) (bitwise op n1 (div2 a) (div2 b)))
+ end.
+
+Definition land (a b : Nat) : Nat := bitwise andb a a b.
+Definition lor (a b : Nat) : Nat := bitwise orb (max a b) a b.
+Definition ldiff (a b : Nat) : Nat := bitwise (fun b b1 => andb b (negb b1)) a a b.
+Definition lxor (a b : Nat) : Nat := bitwise xorb (max a b) a b.
 
--- src/test/resources/scala/of/coq/generated/code/CoqInitNat.scala
+++ src/test/resources/scala/of/coq/generated/code/CoqInitNat.scala
@@ -19,6 +19,7 @@ object CoqInitNat {
       case Zero => m
       case S(p) => S(add(p)(m))
     }
+  def double(n: Nat): Nat = add(n)(n)
   def mul(n: Nat)(m: Nat): Nat =
     n match {
       case Zero => Zero
@@ -29,5 +30,138 @@ object CoqInitNat {
       case (S(k), S(l)) => sub(k)(l)
       case (_, _) => n
     }
+  def eqb(n: Nat)(m: Nat): Boolean =
+    (n, m) match {
+      case (Zero, Zero) => true
+      case (Zero, S(_)) => false
+      case (S(_), Zero) => false
+      case (S(n1), S(m1)) => eqb(n1)(m1)
+    }
+  def leb(n: Nat)(m: Nat): Boolean =
+    (n, m) match {
+      case (Zero, _) => true
+      case (_, Zero) => false
+      case (S(n1), S(m1)) => leb(n1)(m1)
+    }
+  def ltb(n: Nat)(m: Nat) = leb(S(n))(m)
+  def compare(n: Nat)(m: Nat): comparison =
+    (n, m) match {
+      case (Zero, Zero) => Eq
+      case (Zero, S(_)) => Lt
+      case (S(_), Zero) => Gt
+      case (S(n1), S(m1)) => compare(n1)(m1)
+    }
+  def max(n: Nat)(m: Nat): Nat =
+    (n, m) match {
+      case (Zero, _) => m
+      case (S(n1), Zero) => n
+      case (S(n1), S(m1)) => S(max(n1)(m1))
+    }
+  def min(n: Nat)(m: Nat): Nat =
+    (n, m) match {
+      case (Zero, _) => Zero
+      case (S(n1), Zero) => Zero
+      case (S(n1), S(m1)) => S(min(n1)(m1))
+    }
+  def even(n: Nat): Boolean =
+    n match {
+      case Zero => true
+      case S(Zero) => false
+      case S(S(n1)) => even(n1)
+    }
+  def odd(n: Nat): Boolean = negb(even(n))
+  def pow(n: Nat)(m: Nat): Nat =
+    m match {
+      case Zero => S(Zero)
+      case S(m) => mul(n)(pow(n)(m))
+    }
+  def tail_add(n: Nat)(m: Nat): Nat =
+    n match {
+      case Zero => m
+      case S(n) => tail_add(n)(S(m))
+    }
+  def tail_addmul(r: Nat)(n: Nat)(m: Nat): Nat =
+    n match {
+      case Zero => r
+      case S(n) => tail_addmul(tail_add(m)(r))(n)(m)
+    }
+  def divmod(x: Nat)(y: Nat)(q: Nat)(u: Nat): (Nat, Nat) =
+    x match {
+      case Zero => (q, u)
+      case S(x1) => u match {
+        case Zero => divmod(x1)(y)(S(q))(y)
+        case S(u1) => divmod(x1)(y)(q)(u1)
+      }
+    }
+  def div(x: Nat)(y: Nat): Nat =
+    y match {
+      case Zero => y
+      case S(y1) => fst(divmod(x)(y1)(Zero)(y1))
+    }
+  def modulo(x: Nat)(y: Nat) =
+    y match {
+      case Zero => y
+      case S(y1) => sub(y1)(snd(divmod(x)(y1)(Zero)(y1)))
+    }
+  def gcd(a: Nat)(b: Nat): Nat =
+    a match {
+      case Zero => b
+      case S(a1) => gcd(modulo(b)(S(a1)))(S(a1))
+    }
+  def square(n: Nat): Nat = mul(n)(n)
+  def sqrt_iter(k: Nat)(p: Nat)(q: Nat)(r: Nat): Nat =
+    k match {
+      case Zero => p
+      case S(k1) => r match {
+        case Zero => sqrt_iter(k1)(S(p))(S(S(q)))(S(S(q)))
+        case S(r1) => sqrt_iter(k1)(p)(q)(r1)
+      }
+    }
+  def sqrt(n: Nat): Nat = sqrt_iter(n)(Zero)(Zero)(Zero)
+  def log2_iter(k: Nat)(p: Nat)(q: Nat)(r: Nat): Nat =
+    k match {
+      case Zero => p
+      case S(k1) => r match {
+        case Zero => log2_iter(k1)(S(p))(S(q))(q)
+        case S(r1) => log2_iter(k1)(p)(S(q))(r1)
+      }
+    }
+  def log2(n: Nat): Nat = log2_iter(pred(n))(Zero)(S(Zero))(Zero)
+  def iter[A](n: Nat)(f: A => A)(x: A): A =
+    n match {
+      case Zero => x
+      case S(n0) => f(iter(n0)(f)(x))
+    }
+  def div2(n: Nat): Nat =
+    n match {
+      case Zero => Zero
+      case S(Zero) => Zero
+      case S(S(n1)) => S(div2(n1))
+    }
+  def testbit(a: Nat)(n: Nat): Boolean =
+    n match {
+      case Zero => odd(a)
+      case S(n) => testbit(div2(a))(n)
+    }
+  def shiftl(a: Nat)(n: Nat): Nat =
+    n match {
+      case Zero => a
+      case S(n0) => double(shiftl(a)(n0))
+    }
+  def shiftr(a: Nat)(n: Nat): Nat =
+    n match {
+      case Zero => a
+      case S(n0) => div2(shiftr(a)(n0))
+    }
+  def bitwise(op: Boolean => Boolean => Boolean)(n: Nat)(a: Nat)(b: Nat): Nat =
+    n match {
+      case Zero => Zero
+      case S(n1) => add(if (op(odd(a))(odd(b))) S(Zero)
+      else Zero)(mul(S(S(Zero)))(bitwise(op)(n1)(div2(a))(div2(b))))
+    }
+  def land(a: Nat)(b: Nat): Nat = bitwise(andb)(a)(a)(b)
+  def lor(a: Nat)(b: Nat): Nat = bitwise(orb)(max(a)(b))(a)(b)
+  def ldiff(a: Nat)(b: Nat): Nat = bitwise(b => b1 => andb(b)(negb(b1)))(a)(a)(b)
+  def lxor(a: Nat)(b: Nat): Nat = bitwise(xorb)(max(a)(b))(a)(b)
 }
 
