659cb8e09bb3820f3df0a6f01f470baa330b9d93
Joseph Bakouny <youssef.bakouny@usj.edu.lb>
2019-04-15 15:44:32 +0300

    Add fold_left and fold_right to Scallina's standard library

--- src/main/resources/scala/of/coq/lang/MoreLists.scala
+++ src/main/resources/scala/of/coq/lang/MoreLists.scala
@@ -20,4 +20,10 @@ object MoreLists {
 
   def flat_map[A, B](f: A => List[B], l: List[A]): List[B] = l.flatMap(f)
   def flat_map[A, B] = (f: A => List[B]) => (l: List[A]) => l.flatMap(f)
+
+  def fold_left[A, B](op: A => B => A, l: List[B], zero: A): A = l.foldLeft(zero)((x: A, y: B) => op(x)(y))
+  def fold_left[A, B] = (op: A => B => A) => (l: List[B]) => (zero: A) => l.foldLeft(zero)((x: A, y: B) => op(x)(y))
+
+  def fold_right[A, B](op: B => A => A, zero: A, l: List[B]): A = l.foldRight(zero)((x: B, y: A) => op(x)(y))
+  def fold_right[A, B] = (op: B => A => A) => (zero: A) => (l: List[B]) => l.foldRight(zero)((x: B, y: A) => op(x)(y))
 }
